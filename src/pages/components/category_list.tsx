18a1f40f4bb075af166812854292a302b77087b2
Mengheang <mengheangrat@icloud.com>
2023-04-01 18:21:32 +0700

    fixed style

--- src/pages/components/category_list.tsx
+++ src/pages/components/category_list.tsx
@@ -3,7 +3,6 @@ import _ from 'lodash';
 
 import useCategories from '@/hooks/useCategories';
 import { Category } from '@/types/Item';
- 
 
 const CategoryList = (props: any) => {
   const { query, onClick = () => {} } = props;
