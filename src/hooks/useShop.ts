fd8857acd1975d80de9d366294ff37a250cb1c60
Mengheang <mengheangrat@icloud.com>
2023-09-25 03:58:45 +0000

    remove log

--- src/hooks/useShop.ts
+++ src/hooks/useShop.ts
@@ -7,7 +7,6 @@ const getShopInfo = async (props: any) => {
   const { data } = await AxiosUtil.get(
     `${process.env.NEXT_PUBLIC_API_URL}/user/shop/${shop}`
   );
-  console.log(data);
   return data.data;
 };
 
