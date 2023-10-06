06184535403824369358a8c093507b5207dacb74
Mengheang <mengheangrat@icloud.com>
2023-10-06 04:12:03 +0000

    add analytic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,26 @@
+/* eslint-disable */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import Script from 'next/script';
 import { AppConfig } from '@/utils/AppConfig';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
+  
+  script = ` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+  page_path: window.location.pathname,
+  });`;
+
   render() {
     return (
       <Html lang={AppConfig.locale}>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script strategy="lazyOnload">{this.script}</Script>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
